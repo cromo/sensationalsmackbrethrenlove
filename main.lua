2dd07bd57d490ec6dce9d0427d772de55b61a65f
Cristián Romo <cristian.g.romo@gmail.com>
2014-02-02 20:56:06 -0600

    Wrap up the basic spritesheet/animation implementation.
    Timing information still needs to be added to that animation frames for
    this to really be usable. That entails an update to the
    Animation.frameAt function for looking up which frame to display, but
    otherwise this looks ready to be used. It is also in main.lua for the
    time being, and will be moved out when working.

--- main.lua
+++ main.lua
@@ -2,23 +2,68 @@ __ = require "underscore"
 require "input"
 require "events"
 
+sheets = {}
+
+local setViewportOf = function(self, quad, metatilePosition)
+  quad:setViewport(
+      metatilePosition[1] * self.metatileSize[1],
+      metatilePosition[2] * self.metatileSize[2],
+      self.metatileSize[1], self.metatileSize[2])
+end
+
+newSheet = function(name, path, metatileSize)
+  if sheets[name] then
+    print(string.format(
+        "warning: sheet name reused: \"%s\" was originally loaded from \"%s\" will be replaced by \"%s\"",
+        name, sheets[name].path, path))
+  end
+  sheets[name] = {
+    name = name,
+    path = path,
+    image = love.graphics.newImage(path),
+    metatileSize = metatileSize,
+    setViewportOf = setViewportOf
+  }
+end
+
+Animation = {}
+
+Animation.frameAt = function(self, t)
+  local frameOffset = self.frames[math.ceil(t % #self.frames)]
+  sheets[self.sheetName]:setViewportOf(self.frame, frameOffset)
+end
+
+Animation.draw = function(self, x, y, r, sx, sy, ox, oy, kx, ky)
+  love.graphics.drawq(sheets[self.sheetName].image, self.frame, x, y, r, sx, sy, ox, oy, kx, ky)
+end
+
+Animation.update = function(self, dt)
+  self.age = self.age + dt
+  self:frameAt(self.age)
+end
+
+newAnimation = function(sheetName, frames)
+  local o = Animation
+  o.sheetName = sheetName
+  o.frames = frames
+  o.frame = love.graphics.newQuad(
+      0, 0, sheets[sheetName].metatileSize[1], sheets[sheetName].metatileSize[2],
+      sheets[sheetName].image:getWidth(), sheets[sheetName].image:getHeight())
+  o.age = 0
+  return o
+end
+
 function love.load()
-  sheet = love.graphics.newImage("test sprite sheet.png")
-  frame = love.graphics.newQuad(0, 0, 32, 32, 64, 64)
-  age = 0
-  width = 32
-  height = 32
-  frames = {{0, 0}, {1, 0}, {0, 1}, {1, 1}}
+  newSheet("dev.animation", "test sprite sheet.png", {32, 32})
+  testAnimation = newAnimation("dev.animation", {{0, 0}, {1, 0}, {0, 1}, {1, 1}})
 end
 
 function love.draw()
-  love.graphics.drawq(sheet, frame, 0, 0)
+  testAnimation:draw(80, 180)
 end
 
 function love.update(dt)
-  age = age + dt
-  local frameOffset = frames[math.ceil(age % #frames)]
-  frame:setViewport(frameOffset[1] * width, frameOffset[2] * height, width, height)
+  testAnimation:update(dt)
   events.cause('time', {timeDelta = dt})
   events.notify()
 end
